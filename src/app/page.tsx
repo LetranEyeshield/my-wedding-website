aaa9ae103e1ebd38e35fe131a0f059c195a94126
YORIICHI <rosalinmichaelcris@gmail.com>
2025-05-31 23:45:20 +0800

    finished banner, story, continue gallery

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   const [showRSVP, setShowRSVP] = useState(false);
 
   //FOR GALLERY
-  const IMAGES = Array.from({ length: 25 }, (_, i) => ({
+  const IMAGES = Array.from({ length: 10 }, (_, i) => ({
     // src: `/images/gallery/photo-${i + 1}.jpg`,
     src: `/images/gallery/${i + 1}.jpg`,
   }));
@@ -205,7 +205,7 @@ export default function Home() {
 
       {/* Banner */}
       <section id="banner" className="mx-auto flex flex-column justify-center">
-        <h2 className="dancing-script shadow">
+        <h2 className="dancing-script shadow-2">
           "Two Hearts, One Soul, Endless Love"
         </h2>
         <img
@@ -216,8 +216,8 @@ export default function Home() {
       </section>
 
       {/* Our Story */}
-      <section className="mb-12">
-        <h2 className="text-3xl font-semibold mb-4">Our Story</h2>
+      <section id="story" className="p-12">
+        <h2 className="text-5xl font-semibold mb-4">Our Story</h2>
         <p>
           We met in college, and from that moment on, our journey has been full
           of love, laughter, and adventure. We’re excited to share our special
@@ -226,23 +226,34 @@ export default function Home() {
       </section>
 
       {/* Event Details */}
-      <section className="mb-12">
-        <h2 className="text-3xl font-semibold mb-4">Event Details</h2>
-        <ul className="list-disc list-inside space-y-1">
+      <section id="details" className="p-12">
+        <h2 className="text-5xl font-semibold mb-4">Event Details</h2>
+        <ul className="list-disc list-inside space-y-1 pl-4">
           <li>
-            <strong>Date:</strong> July 15, 2025
+            <strong>Date:</strong> September 15, 2025
           </li>
           <li>
-            <strong>Time:</strong> 4:00 PM
+            <strong>Time:</strong> 10:00 AM
           </li>
           <li>
-            <strong>Venue:</strong> Sunny Gardens, 123 Wedding Lane
+            <strong>Church:</strong> Minor Basilica of Our Lady of Manaoag
+          </li>
+          <li>
+            <strong>Venue:</strong> The Monarch Hotel, 29C6+83C, 2, Calasiao,
+            2418 Pangasinan
           </li>
         </ul>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3834.8670213221053!2d120.35772917437438!3d16.020436440743854!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x339142b8c85ec8b9%3A0x6a57e2f53dc0d2c3!2sThe%20Monarch%20Hotel!5e0!3m2!1sen!2sph!4v1748705070728!5m2!1sen!2sph"
+          style={{ border: 0 }}
+          allowFullScreen
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
       </section>
 
       {/* Gallery */}
-      <section className="mb-12">
+      <section id="gallery" className="px-12 py-8">
         {/* <h2 className="text-3xl font-semibold mb-6">Gallery</h2>
         <div className="grid grid-cols-3 gap-4">
           {[
@@ -259,8 +270,8 @@ export default function Home() {
             />
           ))}
         </div> */}
-        <div className="py-10">
-          <h2 className="text-2xl font-bold mb-4 text-center">Gallery</h2>
+        <div className="">
+          <h2 className="text-5xl font-semibold mb-12">Gallery</h2>
 
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 px-4">
             {paginatedImages.map((img, idx) => (
